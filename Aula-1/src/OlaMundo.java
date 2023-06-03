ad6e6515266d4a920e27611a9a685fe9ec7df363
luigiInt1 <luigisantostk@gmail.com>
2023-06-03 15:01:46 -0300

    Atualizando a primeira aula

--- Aula-1/src/OlaMundo.java
+++ Aula-1/src/OlaMundo.java
@@ -4,9 +4,8 @@ public class OlaMundo {
     public static void main(String[] args) {
         
         //Todos os Códigos de Lógica fircaram aqui
-        String anoNascimento = JOptionPane.showInputDialog(null, "Qual o ano que você NASCEU??");
-        int anoNumerico = Integer.parseInt(anoNascimento);
-        JOptionPane.showMessageDialog(null,  "Sua idade é " + (2023 - anoNumerico) );
+        
+        JOptionPane.showMessageDialog(null,  "Hello Word" );
         
     }
 }

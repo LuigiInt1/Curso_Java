0ab3bee30086749a2582f40577b1e5f02066a8dc
LUIGI DOS SANTOS BERNARDO <luigi_bernardo@estudante.sesisenai.org.br>
2023-06-17 09:05:48 -0300

    Bla

---
+++ Aula-2/src/Aula_2.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class Aula_2 {
+    public static void main(String[] args) {
+        
+    JOptionPane.showMessageDialog(null, "Olá");
+    System.out.println("Seja bem vindo");
+    }
+}

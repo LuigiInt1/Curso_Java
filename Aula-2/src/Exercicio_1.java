5c8dc73c991df0547fdf54294c2c7caf18f73865
LUIGI DOS SANTOS BERNARDO <luigi_bernardo@estudante.sesisenai.org.br>
2023-06-17 12:04:59 -0300

    Atualizando aula_2 IF/Else

---
+++ Aula-2/src/Exercicio_1.java
@@ -0,0 +1,30 @@
+import java.text.DecimalFormat;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio_1 {
+    public static void main(String[] args) {
+        String pesoString = JOptionPane.showInputDialog(null,"Qual seu peso em KG?"); 
+        String alturaString = JOptionPane.showInputDialog(null,"Qual sua altura?");
+        int pesoNum = Integer.parseInt(pesoString);
+        double alturaNum = Double.parseDouble(alturaString);
+        double IMC = pesoNum / (alturaNum * 2);
+        DecimalFormat transformador = new DecimalFormat("#0.0");
+        String IMCArredondado = transformador.format(IMC);
+        System.out.println(IMC);
+        
+        if(IMC <18.5)
+        JOptionPane.showMessageDialog(null, IMCArredondado + " você está abaixo do peso");
+        else if (IMC >= 18.5 && IMC <= 24.9 )
+        JOptionPane.showMessageDialog(null,IMCArredondado + " você está no peso ideal");
+        else if (IMC >= 25.0 && IMC <= 29.9 )
+        JOptionPane.showMessageDialog(null,IMCArredondado + " voce está acima do peso");
+        else if (IMC >= 30.0 && IMC <= 34.9 )
+        JOptionPane.showMessageDialog(null,IMCArredondado + " obesidade classe I");
+        else if (IMC >= 35.0 && IMC <= 39.9 )
+        JOptionPane.showMessageDialog(null,IMCArredondado + " obesidade classe II");
+        else
+        JOptionPane.showMessageDialog(null,IMCArredondado + " obesidade classe III");
+        
+    }
+}
--- Aula-2/src/IF_ELSE.java
+++ Aula-2/src/IF_ELSE.java
@@ -3,8 +3,11 @@ import javax.swing.JOptionPane;
 public class IF_ELSE {
     public static void main(String[] args) {
         //idade();
-        votacao();
+        //votacao();
+        //ParOuImpar();
+        //condicional_E();
     }
+
     public static void idade(){
         String idade = JOptionPane.showInputDialog(null,"Qual a Sua idade?");
         int idadeNum = Integer.parseInt(idade);
@@ -14,6 +17,8 @@ public class IF_ELSE {
         JOptionPane.showMessageDialog(null,"Você é maior de idade"  );
 
     }
+
+
     public static void votacao(){
         String idade = JOptionPane.showInputDialog(null,"Qual a Sua idade?");
         int idadeNum = Integer.parseInt(idade);
@@ -26,4 +31,33 @@ public class IF_ELSE {
         else 
         JOptionPane.showMessageDialog(null,"voto OPCIONAL");
     }
+
+
+    public static void ParOuImpar(){
+
+        String numero = JOptionPane.showInputDialog(null,"Digite um valor");
+        int numeroNum = Integer.parseInt(numero);
+        int resto = numeroNum % 2;
+        if(resto == 0)
+        JOptionPane.showMessageDialog(null,"O número que você digitou é par");
+        else
+        JOptionPane.showMessageDialog(null,"O número que você digitou é impar");
+    }
+     
+    
+    public static void condicional_E(){
+        String usuario = "Luigi";
+        String senha = "bla";
+        String digitarUsuario = JOptionPane.showInputDialog(null,"Qual seu nome de usuário?");
+        String digitarSenha = JOptionPane.showInputDialog(null,"Digite sua senha");
+        if(usuario.equals(digitarUsuario) && senha.equals(digitarSenha) )
+        JOptionPane.showMessageDialog(null,"Login efetuado");
+        else
+        JOptionPane.showMessageDialog(null,"Senha ou Cadatro incorretos");
+        
+
+     }
+    
+
+
 }
